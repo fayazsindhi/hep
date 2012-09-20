dd8199f53c001216409b07c6de2ca144ad33537f
shen.wei.total@gmail.com <shen.wei.total@gmail.com@9562fc1b-774c-65fb-0b1a-1813085b3cf9>
2012-09-20 15:27:53 +0000

    first

---
+++ trunk/web_app/js/util.js
@@ -0,0 +1,40 @@
+﻿
+function getGlobalVirtualDirectoryName() {
+    var pathName = window.location.pathname;
+    pathName = pathName.substr(1);
+    pathName = pathName.substr(0, pathName.search("/"));
+
+    return "/" + pathName;
+}
+
+function getFactoryUIPath() {
+    return getGlobalVirtualDirectoryName() + "/PatternUI/BLLFactoryUI.ashx";
+}
+
+function getLoginPath() {
+    return getGlobalVirtualDirectoryName() + "/PatternUI/Login.ashx";
+}
+function getEditPasswordPath() {
+    return getGlobalVirtualDirectoryName() + "/PatternUI/EditPassword.ashx";
+}
+
+
+//禁用右键、文本选择功能、复制按键
+
+$(document).bind("contextmenu", function () {
+    return false;
+});
+
+/**
+* @author yukaizhao
+*/
+$.extend($.fn.validatebox.defaults.rules, {
+    /*必须和某个字段相等*/
+    equalTo: {
+        validator: function (value, param) {
+            return $(param[0]).val() == value;
+        },
+        message: '字段不匹配'
+    }
+
+});
